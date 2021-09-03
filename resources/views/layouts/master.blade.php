025d1dfb20f99ea262f4c269a1e249a059de221a
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-09-03 23:59:58 +0700

    Create master.blade.php

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,12 @@
+<title>@yield('title')</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!--bootstrap-->
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+
+<!--icon-->
+<script src="https://kit.fontawesome.com/babb4f3fd7.js" crossorigin="anonymous"></script>
+
+@yield('indexlayout')

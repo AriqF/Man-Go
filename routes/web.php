c3c47773cac9657d89b68bc7bce743f9efb5dc72
muhammadalifh <61005674+muhammadalifh@users.noreply.github.com>
2021-09-04 16:20:02 +0700

    add multi login (admin + user)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('adminHome', [App\Http\Controllers\HomeController::class, 'adminHome'])->name('adminHome')->middleware('is_admin');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

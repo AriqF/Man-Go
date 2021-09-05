acd02015698d5cc6f6420921f58df88fd9d23acb
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-09-05 13:18:21 +0700

    fix image and form balance

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
         <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
             <div class="row">
                 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                    <div class="row">
+                    <div class="row auth-head">
                         <div class="col align-self-start text-left">
                             <h4>Masuk</h4>
                         </div>

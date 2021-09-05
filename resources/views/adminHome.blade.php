832cdb2d5a9062ebedebbb8eb94645c5ee209693
IbnuFA <60996611+IbnuFA@users.noreply.github.com>
2021-09-05 23:11:08 +0700

    kasih warna

--- resources/views/adminHome.blade.php
+++ resources/views/adminHome.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">
                     Dashboard
                 </div>
-                <div class="card-body">
+                <div class="card-body" style="color: #000">
                     Anda Adalah {{Auth::user()->name}}
                 </div>
                 {{-- Tombol Logout --}}

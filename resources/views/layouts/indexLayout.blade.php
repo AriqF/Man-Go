a9d20865b19e804c228a6a197373cf5fe9940953
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-09-05 13:19:15 +0700

    Update indexLayout.blade.php

--- resources/views/layouts/indexLayout.blade.php
+++ resources/views/layouts/indexLayout.blade.php
@@ -1,3 +1,4 @@
+
 <!--navbar here-->
 <nav class="navbar navbar-expand-lg navbar-dark sticky-top" style="background-color: #343090; font-family: 'Maven Pro', sans-serif;">
     <a class="navbar-brand" href="#" style="font-family: 'Poppins', sans-serif; font-size: 1.5em">ManGo</a>
@@ -5,20 +6,39 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
-
-        <div class="form-inline my-2 my-lg-0">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/">Beranda</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Aksi I</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Aksi II</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Aksi III</a>
+            </li>      
+        </ul>
+        <ul class="navbar-nav ml-auto">
             @if (Route::has('login'))
-                @auth
-                    <a class="userLinks" href="{{ url('/dashboard') }}" type="submit">#username</a>
-                @else
-                        <a class="btn-login" type="submit" href="{{ route('login') }}">Login</a>
-                    @if (Route::has('register'))
-                        <a class="btn-register" type="submit" href="{{ route('register') }}">Register</a>
-                    @endif
-                @endauth
-            @endif
+            @auth
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/dashboard') }}" type="submit">#username</a>
+                </li>
+            @else
+                    <li class="nav-item">
+                        <a class="nav-link"  type="submit" href="{{ route('login') }}">Login</a>
+                    </li>
+                @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link"  type="submit" href="{{ route('register') }}">Register</a>
+                    </li>
+                @endif
+            @endauth
+        @endif
+        </ul>
 
-        </div>
     </div>
 </nav>
 @yield('mainContent')

00186b52d7c4b8f5f050ac351360a041fc9f45cb
muhammadalifh <alifw50@gmail.com>
2021-10-05 15:13:10 +0700

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,8 +5,8 @@
     <div class="row justify-content-center">
         <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
             <img class="img-fluid align-items-center" src="{{ asset('img/login.png') }}">
-        </div> 
-        
+        </div>
+
         <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
             <div class="row">
                 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
@@ -23,7 +23,7 @@
                         <div class="form-floating mb-3">
                             <div class="row">
                                 <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                                    <input id="first_name" type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" placeholder="nama depan" value="{{ old('first_name') }}" required autocomplete="first_name" autofocus>            
+                                    <input id="first_name" type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" placeholder="nama depan" value="{{ old('first_name') }}" required autocomplete="first_name" autofocus>
                                     @error('first_name')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -31,7 +31,7 @@
                                     @enderror
                                 </div>
                                 <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                                    <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" placeholder="nama belakang" value="{{ old('last_name') }}" required autocomplete="last_name" autofocus>            
+                                    <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" placeholder="nama belakang" value="{{ old('last_name') }}" required autocomplete="last_name" autofocus>
                                     @error('last_name')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -41,7 +41,7 @@
                             </div>
                         </div>
                         <div class="form-floating mb-3">
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="example@mail.com" value="{{ old('email') }}" required autocomplete="email" autofocus>            
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="example@mail.com" value="{{ old('email') }}" required autocomplete="email" autofocus>
                             @error('email')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -49,7 +49,7 @@
                             @enderror
                         </div>
                         <div class="form-floating mb-3">
-                            <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="name" placeholder="username" value="{{ old('username') }}" required autocomplete="name" autofocus>            
+                            <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="name" placeholder="username" value="{{ old('username') }}" required autocomplete="name" autofocus>
                             @error('username')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -65,7 +65,7 @@
                             @enderror
                         </div>
                         <div class="form-floating mb-3">
-                            <input id="password-confirm" type="password" class="form-control"  placeholder="konfirmasi password"  name="password_confirmation" required autocomplete="new-password"/>
+                            <input id="password-confirm" type="password" class="form-control @error('password') is-invalid @enderror"  placeholder="konfirmasi password"  name="password_confirmation" required autocomplete="new-password"/>
                             @error('password')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -74,15 +74,15 @@
                         </div>
 
                         <input type="hidden" class="form-control" name="is_admin" value="0">
-                        
+
                         <div class="d-grid">
                             <button class="btn-auth" href="{{ __('Register') }}" type="submit">Daftar</button>
                         </div>
                     </form>
                 </div>
-                
+
             </div>
-            
+
         </div>
 
     </div>

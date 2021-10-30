87d2bcf4ce009bfe93ec47318f682e1ffca6df70
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-10-30 13:48:40 +0700

    update FE for create quiz admin

--- resources/views/admin/create-quiz.blade.php
+++ resources/views/admin/create-quiz.blade.php
@@ -8,53 +8,53 @@
         <div class="box-container border-0 shadow">
             <form class="form-horizontal" role="form" action="/quizes" method="POST" enctype="multipart/form-data">
                 {{csrf_field()}}
-            <div class="space-4"></div>
-            <div class="form-group">
-                <label class="col-sm-3 control-label no-padding-right" for="form-field-2"> Quiz Name </label>
-                <div class="col-sm-9">
-                <input type="text" id="form-field-2" placeholder="Quize Name" class="col-xs-10 col-sm-5" name="quiz_name" required="" >
+                <div class="space-4"></div>
+                <div class="form-group">
+                    <label class="col-sm-3 control-label no-padding-right" for="form-field-2"> Quiz Name </label>
+                    <div class="col-sm-9">
+                    <input type="text" id="form-field-2" placeholder="Quize Name" class="col-xs-10 col-sm-5 form-control" name="quiz_name" required="" >
+                    </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <label class="col-sm-3 control-label no-padding-right" for="form-field-2">Description</label>
-                <div class="col-sm-9">
-                    <input type="text" id="form-field-2" placeholder="Descriotion" class="col-xs-10 col-sm-5" name="description" required="" />
+                <div class="form-group">
+                    <label class="col-sm-3 control-label no-padding-right" for="form-field-2">Description</label>
+                    <div class="col-sm-9">
+                        <input type="text" id="form-field-2" placeholder="Descriotion" class="col-xs-10 col-sm-5 form-control" name="description" required="" />
+                    </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <label class="col-sm-3 control-label no-padding-right" for="form-field-2">Quiz Time</label>
-                <div class="col-sm-9">
-                    <input type="text" id="form-field-2" placeholder="00:00"  pattern="[0-9]{2}:[0-9]{2}" class="col-xs-10 col-sm-5" name="quiz_time" required="" title="example 01:00 " />
-                    <span> exm: 00:00</span>
+                <div class="form-group">
+                    <label class="col-sm-3 control-label no-padding-right" for="form-field-2">Quiz Time</label>
+                    <div class="col-sm-9">
+                        <input type="text" id="form-field-2" placeholder="00:00"  pattern="[0-9]{2}:[0-9]{2}" class="col-xs-10 col-sm-5 form-control"
+                         name="quiz_time" required="" title="example 01:00 " aria-describedby="quizTime_help" />
+                    </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <label class="col-sm-5 control-label no-padding-right" for="form-field-2">Number Of Question View for User</label>
-                <div class="col-sm-9">
-                    <input type="text" id="form-field-5" placeholder="How many question view for this quiz " class="col-xs-10 col-sm-5" name="number_of_question" required="" />
+                <div class="form-group">
+                    <label class="col-sm-5 control-label no-padding-right" for="form-field-2">Number Of Question View for User</label>
+                    <div class="col-sm-9">
+                        <input type="text" id="form-field-5" placeholder="How many question view for this quiz " class="col-xs-10 col-sm-5 form-control" name="number_of_question" required="" />
+                    </div>
                 </div>
-            </div>
-            <div class="space-4"></div>
-            <div class="clearfix form-actions">
-                <div class="col-md-offset-3 col-md-9">
-                    <button class="btn btn-info" type="submit">
-                        <i class="ace-icon fa fa-check bigger-110"></i>
-                        Submit
-                    </button>
-            
-                    &nbsp; &nbsp; &nbsp;
-                    <button class="btn" type="reset">
-                        <i class="ace-icon fa fa-undo bigger-110"></i>
-                        Reset
-                    </button>
+                <div class="space-4"></div>
+                <div class="clearfix form-actions" style="margin-bottom: 16px">
+                    <div class="col-md-offset-3 col-md-9">
+                        <button class="btn btn-info" type="submit">
+                            <i class="ace-icon fa fa-check bigger-110"></i>
+                            Submit
+                        </button>
+                
+                        &nbsp; &nbsp; &nbsp;
+                        <button class="btn btn-danger" type="reset">
+                            <i class="ace-icon fa fa-undo bigger-110"></i>
+                            Reset
+                        </button>
+                    </div>
                 </div>
-            </div>
-            <div class="hr hr-24"></div>
+                <div class="hr hr-24"></div>
             </form>
 
-            <div class="col-md-12">
+            <div class="col-md-12 ">
                 {{$quizes->links()}}
-                <table class="table table-bordered" id="datatables">
+                <table class="table table-bordered text-white" id="datatables">
                    <thead>
                        <tr>
                            <th>Sl</th>

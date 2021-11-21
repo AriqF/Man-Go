2cd479d3e99def4f190b023e3031e9fdc254e418
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-11-21 14:27:02 +0700

    Update ratingSeeder.php

--- database/seeders/ratingSeeder.php
+++ database/seeders/ratingSeeder.php
@@ -15,9 +15,9 @@ class ratingSeeder extends Seeder
     public function run()
     {
         $ratingRecords = [
-            ['id' =>1, 'user_id'=>1, 'comments'=>'Mantap Jiwaa', 'rating'=>4, 'status'=>0],
-            ['id' =>2, 'user_id'=>2, 'comments'=>'Lumayan Jiwaa', 'rating'=>2, 'status'=>0],
-            ['id' =>3, 'user_id'=>3, 'comments'=>'Sad Jiwaa', 'rating'=>1, 'status'=>0],
+            ['user_id'=>1, 'comments'=>'Mantap Jiwaa', 'rating'=>4, 'status'=>0],
+            ['user_id'=>2, 'comments'=>'Lumayan Jiwaa', 'rating'=>2, 'status'=>0],
+            ['user_id'=>3, 'comments'=>'Sad Jiwaa', 'rating'=>1, 'status'=>0],
         ];
 
         Rating::insert($ratingRecords);
